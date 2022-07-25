b0eeddf1956bc0411a85ec6134fdf271faa4425d
TomatoGremlin <101596047+TomatoGremlin@users.noreply.github.com>
2022-07-25 13:18:18 +0300

    Update test2_4.c

--- test2_4.c
+++ test2_4.c
@@ -1,5 +1,5 @@
 1. find /home -name "*.h"
 2. grep -cr "main()" ~
-3.1 echo | grep -E "^[0|+359]8[789]???????$"
-3.2 wc | grep -E !"^[0|+359]8[789]???????$"
-3.3 cat phones.info | grep -E "^*7*$" < phones.vivacom.data
+3.1 echo | grep -E "^[0|+359]8[789]???????$" phones.info
+3.2 wc | grep -E !"^[0|+359]8[789]???????$" phones.info
+3.3 cat phones.info | grep -E "^*7*$" phones.info < phones.vivacom.data
